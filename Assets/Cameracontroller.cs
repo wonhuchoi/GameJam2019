c26796911105d66528db6aaafa8ba3498429c1ff
Sammy <bigsaem@gmail.com>
2019-02-16 23:56:07 -0800

    completed building structure

--- Assets/Cameracontroller.cs
+++ Assets/Cameracontroller.cs
@@ -7,13 +7,47 @@ public class Cameracontroller : MonoBehaviour {
 	public List<Transform> camangleList = new List<Transform>();  //asign all transforms in inspector, including the initial position
 	private int clickCounter = 0;
 	private bool clicked;
+	public float speed = 5.0f;
+
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) // this will rgister a click anywhere on the screen, you need to ray cast on your button
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(new Vector3(speed * Time.deltaTime*3, 0, 0));
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(new Vector3(-speed * Time.deltaTime * 3, 0, 0));
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Translate(new Vector3(0, 0 , -speed * Time.deltaTime * 3));
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate(new Vector3(0,0, speed * Time.deltaTime * 3));
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(new Vector3(0, -speed * Time.deltaTime * 3, 0));
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(new Vector3(0, speed * Time.deltaTime * 3, 0));
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(new Vector3(0, -speed * Time.deltaTime*10, 0));
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(new Vector3(0, speed * Time.deltaTime*10, 0));
+		}
+		/*if (Input.GetMouseButtonDown(0)) // this will rgister a click anywhere on the screen, you need to ray cast on your button
 		{
 			clickCounter++;
 			clicked = true;
@@ -23,6 +57,6 @@ public class Cameracontroller : MonoBehaviour {
 		{
 			transform.position = Vector3.Lerp(transform.position, camPositionList[clickCounter].position, Time.deltaTime);
 			if (transform.position == camPositionList[clickCounter].position) clicked = false;
-		}
+		}*/
 	}
 }
